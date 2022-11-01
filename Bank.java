f802445f2d3ea3beb55fff8b9a6c1d9c1b0519d8
DENDI SUHADA IRAWAN <113730409+dendisuhada@users.noreply.github.com>
2022-11-01 17:08:08 +0700

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class Bank {
+    int saldo = 100000;
+   public void ambilMoney(int c){
+       saldo = saldo - c;
+       if(saldo <= saldo){
+           System.out.println("saldo Kurang");
+       }else
+    System.out.println("Saldo saat ini : " + saldo);
+            
+   }
+   public void simpanMoney(int c){
+       saldo = saldo + c;
+     System.out.println("Saldo saat ini : " + saldo);      
+   }
+    public void getSaldo(){
+    System.out.println("Saldo saat ini : " + saldo);      
+   }  
+}
